3ff8bd561c53babb446713bf03bcc006bf03393e
xavierfrank4 <xavierfrank4@gmail.com>
2022-03-07 03:40:39 +0300

    create main providers and variables terraform files

---
+++ terraform/provider.tf
@@ -0,0 +1,17 @@
+#Terraform Block
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "jumia"
+  region  = "eu-west-2"
+}
