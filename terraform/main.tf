2c091b8a9cdfd79e94483a817a405193fb79d457
xavierfrank4 <xavierfrank4@gmail.com>
2022-03-08 03:49:22 +0300

    create DB security group

--- terraform/main.tf
+++ terraform/main.tf
@@ -174,3 +174,32 @@ resource "aws_instance" "microservice_app" {
   }
 
 }
+
+#Create Database Security Group
+resource "aws_security_group" "rds_sg" {
+  name        = "rds_sg"
+  description = "Allow inbout traffic from microservices"
+  vpc_id      = aws_vpc.jumia_vpc.id
+
+  ingress {
+    description     = "Allow TCP from Microservices"
+    from_port       = 5432
+    to_port         = 5432
+    protocol        = "tcp"
+    security_groups = [aws_security_group.microservice_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "RDS_Database_SG"
+  }
+
+}
+
+#Create Application Load Balancer
